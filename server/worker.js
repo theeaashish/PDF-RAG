57ce332ea2e464646464c00328edb241a079a351
Aashish Joshi <theeaashish2@gmail.com>
2025-08-02 18:43:55 +0000

    docker added qdrant

--- server/worker.js
+++ server/worker.js
@@ -4,6 +4,9 @@ import { QdrantVectorStore } from "@langchain/qdrant";
 import { Document } from "@langchain/core/documents";
 import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
 import { CharacterTextSplitter } from "@langchain/textsplitters";
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const worker = new Worker(
   "file-upload-queue",
@@ -21,12 +24,18 @@ const worker = new Worker(
     //load the pdf
     const loader = new PDFLoader(data.path);
     const docs = await loader.load();
-    const textSplitter = new CharacterTextSplitter({
-      chunkSize: 100,
-      chunkOverlap: 0,
-    });
-    const texts = await textSplitter.splitText(docs[0]);
-    console.log(`\n\nAfter split`, texts);
+
+    //store the pdf in qdrant
+    const client = new QdrantClient({ url: `http://localhost:6333` });
+    const embeddings = new OpenAIEmbeddings({ apiKey: process.env.OPENAI_API_KEY });
+
+
+    // const textSplitter = new CharacterTextSplitter({
+    //   chunkSize: 100,
+    //   chunkOverlap: 0,
+    // });
+    // const texts = await textSplitter.splitText(docs[0]);
+    // console.log(`\n\nAfter split`, texts);
   },
   {
     concurrency: 100,
